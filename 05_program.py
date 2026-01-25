31a931d8a9d3e82e36f67d4083d45d4b8a6b9e6e
Hassan Raza <hrpythonai@gmail.com>
2026-01-25 10:01:54 -0800

    Create 05_program.py

---
+++ 05_program.py
@@ -0,0 +1,12 @@
+#Program to swap two variables(using third variable)
+x=10
+y=20
+temp=x
+x=y
+y=temp
+print("value of x is: ",x, "value of y is: ",y)
+#Program to swap two variables
+x=10
+y=20
+x,y=y,x
+print("value of x is: ",x, "value of y is: ",y)
