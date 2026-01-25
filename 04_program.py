3b5ff9de043fc4eb3582cc5dd7208b6620cb4c45
Hassan Raza <hrpythonai@gmail.com>
2026-01-25 08:51:43 -0800

    Create 04_program.py

---
+++ 04_program.py
@@ -0,0 +1,41 @@
+#Find area of square
+length=float(input("Enter length of the square: "))
+area_of_square=length**2
+print("the area of the square is: ",area_of_square)
+
+#Find area of rectangle
+length=float(input("Enter length of the rectangle: "))
+width=float(input("Enter width of the rectangle: "))
+area_of_rectangle=length*width
+print("the area of the rectangle is: ",area_of_rectangle)
+
+#Find area of triangle
+base=float(input("Enter base of the triangle: "))
+height=float(input("Enter height of the triangle: "))
+area_of_triangle=1/2*(base*height)
+print("the area of the triangle is: ",area_of_triangle)
+
+#Find area of circle
+radius=float(input("Enter radius of circle: "))
+PI=3.14
+area_of_circle=PI*radius**2
+print("the area of the circle is: ",area_of_circle)
+
+#Find area of cylinder
+height=float(input("Enter height of the cylinder: "))
+radius=float(input("Enter radius of cylinder: "))
+PI=3.14
+area_of_cylinder=(2*PI*radius*height)+(2*PI*r**2)
+print("area of cylinder is: ",area_of_cylinder)
+
+
+
+
+
+
+
+
+
+
+
+
