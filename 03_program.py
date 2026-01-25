651dc3c39a2f2293580a4f4e6a38fbbc9f295855
Hassan Raza <hrpythonai@gmail.com>
2026-01-25 08:01:26 -0800

    Create 03_program.py

---
+++ 03_program.py
@@ -0,0 +1,10 @@
+#Program to find the square root of a number
+num=float(input("Enter the number, which to find out square root: "))
+square_root=num**(1/2)
+print("square root of given number is: ", square_root)
+#find square root using math module
+import math
+num=float(input("Enter the number, which to find out square root: "))
+square_root=math.sqrt(num)
+print("square root of given number is: ", square_root)
+
