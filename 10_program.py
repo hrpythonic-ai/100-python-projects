a06145754e8e03e502184f7e5a0c054aec8d3ac5
Hassan Raza <hrpythonai@gmail.com>
2026-01-26 07:27:23 -0800

    Create 10_program.py

---
+++ 10_program.py
@@ -0,0 +1,8 @@
+#Program to check zero, positive number or negative number
+num=float(input("Enter a number: "))
+if num>0:
+  print(num,"is positive number")
+elif num<0:
+  print(num,"is negative number")
+else:
+  print(num,"is zero.")
