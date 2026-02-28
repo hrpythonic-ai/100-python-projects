94c555de10b6700afd95fb1f0176aa17ef1e6666
Hassan Raza <hrpythonai@gmail.com>
2026-02-27 16:43:18 -0800

    Create 16_program.py

---
+++ 16_program.py
@@ -0,0 +1,13 @@
+#Program to display multiplication table 
+num=int(input("Enter a number that whould you want to  print table: "))
+for i in range(11):
+    print(num,"x",i,"=",num*i)
+
+
+
+
+
+
+
+
+
