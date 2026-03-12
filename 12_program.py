ffe22065c2e05aba6eefc07beee207b98909e8bd
Hassan Raza <hrpythonai@gmail.com>
2026-03-12 15:07:05 -0700

    Update 12_program.py

--- 12_program.py
+++ 12_program.py
@@ -7,7 +7,7 @@ else:
 
 #2nd method 
 year=int(input("Enter year: "))
-if (year%4==0 and year%100!=0) 
+if (year%4==0 and year%100!=0): 
   print(year,"is a leap year.")
 elif(year%400==0):
   print(year,"is a leap year.")    
